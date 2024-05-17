3aa4b0bff46707110978307f1d4d3829beb52ad5
FloGeez <florian.etourneau@gmail.com>
2024-05-17 23:15:27 +0200

    feat(webcontainers): First step

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  server: {
+      headers: {
+          "Cross-Origin-Opener-Policy": "same-origin",
+          "Cross-Origin-Embedder-Policy": "require-corp",
+      },
+  },
+});
